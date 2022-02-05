9d0ff52c6d235ca469e7c78560a460888890faf6
張旭 <zx1986@gmail.com>
2022-02-05 22:39:07 +0800

    feat: 將 settings.yaml 設定值的結構做一次轉置再處理
    考慮了另外一種設定值的結構，可以配合 Gitlab library，但 yaml 寫起來繁瑣；
    因此額外做了轉置處理，但在這次 commit 保留繁瑣結構供歷史查閱 (下一個 commit 會刪除)
    
    初始化全新的專案 variables 是可以全部建立進去，
    但一旦該專案已經存在相同的環境變數，會導致後續變數全部建立失敗。
    因為一遇到已經存在的環境變數，流程就會直接 raise error 中斷掉。

--- main.py
+++ main.py
@@ -2,7 +2,10 @@
 
 import gitlab
 import yaml
+import pprint
 import os
+
+pp = pprint.PrettyPrinter(indent=4)
 
 if __name__ == "__main__":
 
@@ -14,22 +17,44 @@ if __name__ == "__main__":
 
     # 從 ENV 取得 Gitlab Project 參數與授權
     # 讀取 yaml 檔案載入要設定的環境變數
+    yaml_file = os.environ['GITLAB_SETTING_FILE']
+    with open(yaml_file, 'r') as stream:
+        try:
+            yaml_settings = yaml.safe_load(stream)
+        except yaml.YAMLError as e:
+            raise e
+
+    # 將 settings.yaml 內的環境變數結構重組
+    # 改成配合 Gitlab library object 的結構
+    # 為何不一開始就設計成 Gitlab library object 的結構？
+    # 因為那個結構寫起來不方便，如果不優化環境變數設定寫法，那這個工具就沒意義了
+    yaml_variables = []
+    for environment in yaml_settings['environments']:
+        for variable in environment['variables']:
+            _var = {}
+            for key in variable:
+                _var['key'] = key
+                _var['value'] = variable[key]
+                _var['environment_scope'] = environment['name']
+                _var['protected'] = environment['protected']
+                _var['masked'] = environment['masked']
+            yaml_variables.append(_var)
 
     # 將既有環境變數跟 yaml 讀取進來的環境變數進行比較
     # 已經存在的環境變數不做任何處理
     # 僅新增尚未存在 key 與 environment_scope 搭配組合
     for yaml_var in yaml_variables:
-        for project_var in project.variables.list():
-            if yaml_var.key == project_var.key and yaml_var.environment_scope == project_var.environment_scope:
-                print("Key: %s [%s] existed." % (yaml_var.key, yaml_var.environment_scope))
-            else:
-                project.variables.create({
-                    'key': yaml_var.key,
-                    'value': yaml_var.value,
-                    'environment_scope': yaml_var.environment_scope,
-                    'protected': yaml_var.protected,
-                    'marked': yaml_var.marked
-                })
+        # for project_var in project.variables.list():
+        #     if yaml_var['key'] == project_var.key and yaml_var['environment_scope'] == project_var.environment_scope:
+        #         print("Key: %s [%s] existed." % (yaml_var.key, yaml_var.environment_scope))
+        #     else:
+        project.variables.create({
+            'key': yaml_var['key'],
+            'value': yaml_var['value'],
+            'environment_scope': yaml_var['environment_scope'],
+            'protected': yaml_var['protected'],
+            'masked': yaml_var['masked']
+        })
 
     for project_var in project.variables.list():
         print("[%s] Key: %s, Value: %s" % (project_var.environment_scope, project_var.key, project_var.value))
