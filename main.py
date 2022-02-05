63fa90f28570b24b29ebdfbfe78d16c546fa555f
張旭 <zx1986@gmail.com>
2022-02-05 22:47:40 +0800

    fix(error): 還是無法正確判斷跳過已經存在的環境變數

--- main.py
+++ main.py
@@ -41,17 +41,17 @@ if __name__ == "__main__":
     # 已經存在的環境變數不做任何處理
     # 僅新增尚未存在 key 與 environment_scope 搭配組合
     for yaml_var in yaml_variables:
-        # for project_var in project.variables.list():
-        #     if yaml_var['key'] == project_var.key and yaml_var['environment_scope'] == project_var.environment_scope:
-        #         print("Key: %s [%s] existed." % (yaml_var.key, yaml_var.environment_scope))
-        #     else:
-        project.variables.create({
-            'key': yaml_var['key'],
-            'value': yaml_var['value'],
-            'environment_scope': yaml_var['environment_scope'],
-            'protected': yaml_var['protected'],
-            'masked': yaml_var['masked']
-        })
+        for project_var in project.variables.list():
+            if yaml_var['key'] == project_var.key and yaml_var['environment_scope'] == project_var.environment_scope:
+                print("Key: %s [%s] existed." % (yaml_var['key'], yaml_var['environment_scope']))
+            else:
+                project.variables.create({
+                    'key': yaml_var['key'],
+                    'value': yaml_var['value'],
+                    'environment_scope': yaml_var['environment_scope'],
+                    'protected': yaml_var['protected'],
+                    'masked': yaml_var['masked']
+                })
 
     for project_var in project.variables.list():
         print("[%s] Key: %s, Value: %s" % (project_var.environment_scope, project_var.key, project_var.value))
