db73a6ec4b54fc647ba72e571c1558d936afaf1a
張旭 <zx1986@gmail.com>
2022-02-06 00:01:53 +0800

    chore: debugging

--- main.py
+++ main.py
@@ -15,9 +15,11 @@ if __name__ == "__main__":
     gl = gitlab.Gitlab(gl_base_url, job_token=gl_job_token)
     project = gl.projects.get(gl_project_id)
 
+    print('---------')
     print(gl_base_url)
     print(gl_job_token)
     print(gl_project_id)
+    print('---------')
 
     # 從 ENV 取得 Gitlab Project 參數與授權
     # 讀取 yaml 檔案載入要設定的環境變數
