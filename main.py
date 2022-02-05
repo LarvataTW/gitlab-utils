8e1947f0f31805298fe44891701a641df1c89a45
張旭 <zx1986@gmail.com>
2022-02-05 21:29:57 +0800

    feat: 鋪墊從 yaml 讀取設定並跟既有環境變數比對的流程

--- main.py
+++ main.py
@@ -3,32 +3,32 @@
 import gitlab
 import os
 
-gitlab_url=''
-gitlab_private_token = ''
-gitlab_project_id = 424
-
-gl = gitlab.Gitlab(gitlab_url, private_token=gitlab_private_token)
-gl.auth()
-
-project = gl.projects.get(gitlab_project_id)
-
-if not project.variables.get('TEST', filter={'environment_scope': '*'}):
-    project.variables.create({
-        'key': 'TEST',
-        'value': 'zx1986',
-        'environment_scope': '*',
-        'protected': 'false',
-        'marked': 'false'
-    })
-
-project.variables.list()
-
 if __name__ == "__main__":
+
+    try:
+        gl = gitlab.Gitlab(os.environ['GITLAB_URL'], private_token=os.environ['GITLAB_PRIVATE_TOKEN'])
+        project = gl.projects.get(os.environ['GITLAB_PROJECT_ID'])
+    except Exception as e:
+        raise 'GitLab authentication failed or Project not found.'
+
     # 從 ENV 取得 Gitlab Project 參數與授權
     # 讀取 yaml 檔案載入要設定的環境變數
-    # 透過 Gitlab API 開始配置環境變數
-    ## 僅新增尚未建立的環境變數
-    ## 已存在同環境同名的變數不要動
-    ## 不要移除其他已經存在的變數
-    ## 回報執行結果，注意敏感資訊問題
-    pass
+
+    # 將既有環境變數跟 yaml 讀取進來的環境變數進行比較
+    # 已經存在的環境變數不做任何處理
+    # 僅新增尚未存在 key 與 environment_scope 搭配組合
+    for yaml_var in yaml_variables:
+        for project_var in project.variables.list():
+            if yaml_var.key == project_var.key and yaml_var.environment_scope == project_var.environment_scope:
+                print("Key: %s [%s] existed." % (yaml_var.key, yaml_var.environment_scope))
+            else:
+                project.variables.create({
+                    'key': yaml_var.key,
+                    'value': yaml_var.value,
+                    'environment_scope': yaml_var.environment_scope,
+                    'protected': yaml_var.protected,
+                    'marked': yaml_var.marked
+                })
+
+    for project_var in project.variables.list():
+        print("[%s] Key: %s, Value: %s" % (project_var.environment_scope, project_var.key, project_var.value))
